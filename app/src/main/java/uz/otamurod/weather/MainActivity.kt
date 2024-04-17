8fe237582858bbb2b9352cd345aedcea3c872e78
otamurod <o.safarov@student.inha.uz>
2024-04-17 09:21:33 +0500

    feat: use ViewBinding

--- app/src/main/java/uz/otamurod/weather/MainActivity.kt
+++ app/src/main/java/uz/otamurod/weather/MainActivity.kt
@@ -2,10 +2,13 @@ package uz.otamurod.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.otamurod.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
