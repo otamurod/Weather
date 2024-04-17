b0e6dc9eda662958f3ef3a45136f53aff707e156
otamurod <o.safarov@student.inha.uz>
2024-04-17 09:52:56 +0500

    feat: move files to Presentation Layer

---
+++ presentation/src/main/java/uz/otamurod/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package uz.otamurod.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import uz.otamurod.presentation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
